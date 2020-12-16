abaac639eb01a638e238c68861ad89d94e97932b
x_zhang <x_zhang@cm-group.co.jp>
2020-12-16 16:43:44 +0900

    Revert "fix: secretOrPrivateKey must have a value 3"
    This reverts commit 6371a47c6de7b74e98fdb15cbf99008964b5298f.

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -1,11 +1,12 @@
 # Get the private key from the environment variable
 echo "Setting up DevHub Connection..."
-mkdir keys
-echo "$SFDC_SERVER_KEY" | base64 -d > keys/server.key
+printf $SFDC_SERVER_KEY > server.key
+# mkdir keys
+# echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a MyTP
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a MyTP
 
 #Create a scratch org
 echo "Creating the Scratch Org..."

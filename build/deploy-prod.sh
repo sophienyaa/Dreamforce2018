7aa7b57f0d0289bfcb5f632b7a2733e8295dc085
x_zhang <x_zhang@cm-group.co.jp>
2020-12-17 15:55:30 +0900

    Change DevHub to MyTP

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -3,4 +3,4 @@ echo "Converting to MDAPI format..."
 sfdx force:source:convert -d deploy_prod -r force-app 
 #Deploy to prod & run all tests
 echo "Deploying to production & running all tests..."
-sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 -l RunAllTestsInOrg
+sfdx force:mdapi:deploy -u MyTP -d deploy_prod/ -w -1 -l RunAllTestsInOrg
